ce6e3041f4de85b6abe811916dc7a454f2e9558e
Matheus <149837292+matheuspimentel1@users.noreply.github.com>
2024-09-19 20:53:16 -0300

    Aula 19/09/2024

---
+++ exercicios/condicionais/exercicio1.js
@@ -0,0 +1,18 @@
+// Criando uma função
+
+function comparaNumeros(numero1, numero2) {
+    if (numero1 === numero2){
+        return "Os números são iguais! :D"
+    }else{
+        return "Os números são diferentes :(((("
+    }
+}
+
+// Definir as variáveis de entrada (numero1 e numero2)
+
+let numero1 = 20
+let numero2 = 20
+
+// Chamada da função
+resultado = comparaNumeros(numero1, numero2)
+console.log(resultado)
